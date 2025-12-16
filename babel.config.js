1c2f158ac42f2b54865b904971404679dc746e25
FedeOde <odenafederico@gmail.com>
2025-12-16 03:26:12 -0300

    Fix: Remove redundant babel plugin causing 'loose' mode conflict

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    // === ESTA ES LA LÍNEA QUE NECESITAS AÑADIR ===
-    '@babel/plugin-transform-private-methods',
-  ],
+  plugins: [], // Debe estar vacío
 };
